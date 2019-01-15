52e013c5e8d67905994b85869ad38403af5d7a3d
dogtownoak <david.brandon.ford@gmail.com>
2019-01-14 17:28:01 -0800

    updated user post .find

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,7 +1,7 @@
 // const express = require("express");
 // const router = express.Router();
 
-const mongoose = require("../models/Post");
+const Post = require("../models/Post");
 const db = require("../models")
 // const Post = mongoose.model("Post");
 
@@ -9,18 +9,14 @@ const db = require("../models")
 
 module.exports = {
 
-  userPosts: (req,res) => {
-    console.log("hitting get posts");
-    db.Post.findOne({ user: req.params.userId }, (err, result) => {
-      if (err) {
-        return res.status(999).json({ err });
-      }
-      res.status(200).json( {result })
-    })
-  }
+userPosts: (req,res) => {
+  console.log("hitting get posts");
+  db.Post.find({ user: { _id: req.params.userId }}, (err, result) => {
+    if (err) {
+      return res.status(999).json({ err });
+    }
+    res.status(200).json( {result })
+  })
+}
 }
 
-
-// router.get("/posts", (req, res) => {
-//   Post.find({}).then(posts => res.json(posts));
-// });
